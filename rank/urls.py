8e3659a1ed4cd72f7c13e39f232a9c8d0958d50c
hustyuantao <yuan-yu-tao@163.com>
2013-11-09 23:14:15 +0800

    add top100listview

--- rank/urls.py
+++ rank/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from rank import views
 
 urlpatterns = patterns('',
-	url(r'^top100/(?P<nth>\d+)/$', views.list)
+	url(r'^top100/(?P<nth>\d+)/$', views.top100list),
+	url(r'^top500/(?P<nth>\d+)/$', views.top500list),
 )
--- rank/views.py
+++ rank/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rank.models import TopOneHundred
 
 # Create your views here.
+def top100list(request, nth):
+	top100_list = TopOneHundred.objects.all().filter(nth=nth).order_by('rank')
+	context = {'top100_list': top100_list}
+	return render(request, 'rank/top100list.html', context)
+
+def top500list(request, nth):
+	top100_list = TopOneHundred.objects.all().filter(nth=nth).order_by('rank')
+	context = {'top100_list': top100_list}
+	return render(request, 'rank/top100list.html', context)
